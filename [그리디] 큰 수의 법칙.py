22488734c4c01bb65e982573b36248020d4f13eb
SeungJu Hwang <platinadark@gmail.com>
2021-10-25 17:31:46 +0900

    21.10.25

--- [그리디] 큰 수의 법칙.py
+++ [그리디] 큰 수의 법칙.py
@@ -5,13 +5,14 @@
   첫째 줄에 배열의 크기 N(2 <= N <= 1,000), 숫자가 더해지는 횟수 M(1 <= M <= 10,000), 특정한 인덱스 최대 연속 횟수 K(1 <= K <= 10,000)의 자연수가 주어지며, 각 자연수는 공백으로 구분한다.
   둘째 줄에 N개의 자연수가 주어진다. 각 자연수는 공백으로 구분한다. 단, 각각의 자연수는 1 이상 10,000 이하의 수로 주어진다.
   입력으로 주어지는 K는 항상 M보다 작거나 같다.
-# 출력 조건 첫째 줄에 큰 수의 법칙에 따라 더해진 답을 출력한다.
+# 출력 조건 
+  첫째 줄에 큰 수의 법칙에 따라 더해진 답을 출력한다.
 # 시간 제한 1초, 메모리 제한 128MB
 '''
 
 '''
-# 1차 풀이 >> O(n)
-N, M, K = list(map(int, input().split())) 
+# 1차 풀이 >> O(N)
+N, M, K = map(int, input().split())) 
 data = list(map(int, input().split()))
 data.sort(reverse = True)
 count = 0
@@ -29,7 +30,7 @@ print(result)
 '''
 
 # 2차 풀이 >> O(1)
-N, M, K = list(map(int, input().split()))
+N, M, K = map(int, input().split())
 data = list(map(int, input().split()))
 data.sort(reverse= True)
 count = int(M / (K + 1)) * K + M % (K + 1)
